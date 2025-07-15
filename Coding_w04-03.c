c64a9af4d11657d250dc501bf18cad5d9a9e7ba4
Kunasin6812022611065 <s6812022611065@email.kmutnb.ac.th>
2025-07-15 18:21:20 +0700

    คณิตแท้ไม่มีคำปลอบใจ

---
+++ Coding_w04-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    char Linkin_Park = 'K';
+    int Limp_Bizkit = 17;
+    float MOjojojo = 65.34;
+    char POwer_Puff_Girl[] = "Male";
+
+    printf("nameInitial = %c\n", Linkin_Park);
+    printf("age = %d\n", Limp_Bizkit);
+    printf("weight = %f\n", MOjojojo);
+    printf("gender = %s\n", POwer_Puff_Girl);
+    return 0;
+
+}
